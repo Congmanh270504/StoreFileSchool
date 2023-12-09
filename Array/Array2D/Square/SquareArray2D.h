14cb2df59bcbcda24a19e30a9de5f9e891fae532
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2023-12-09 14:42:26 +0700

    Update SquareArray2D.h

--- Array/Array2D/Square/SquareArray2D.h
+++ Array/Array2D/Square/SquareArray2D.h
@@ -15,17 +15,13 @@ void createRandomArray2D(int**& a, int& m);
 void readArray2DFile(const char* filename, int**& a, int& m);
 void writeFileArray2D(const char* filename, int** a, int m);
 
-void getNumberMainLine(int**& a, int& m);
-void getNumberParallelMainLine(int**& a, int& m);
+
 
 int findMax(int*& b, int n);
 int findMin(int*& b, int n);
 
 void sortZicZac(int** a, int& m);
-void interchangeSortMainLineIncrease(int**& a, int& m);
 
-void setArraySortFirstEvenLastOdd(int**& a, int& m);
-int checkSymmetryArray2D(int**& a, int& m);
 
 void freeMatrix(int** &a, int m);
 #endif 
